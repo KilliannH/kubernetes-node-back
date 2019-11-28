473862d6c5af8baef0173f7d7cb2530fce8c2fe2
KilliannH <killiannhervagault@gmail.com>
2019-11-28 12:03:34 +0100

    code base

---
+++ server.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var Sequelize = require('sequelize');
+
+app.use(bodyParser.json());
+
+//Connect to mySQL
+const sequelize = new Sequelize('mysql://root:root@localhost:3306/vote-db', {sync: true});
+
+//Let's checkout connection
+sequelize
+    .sync()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+//Init model (in this same file because it's a simple single page app)
+const Vote = sequelize.define('vote', {
+    value: {
+        type: Sequelize.STRING
+    }
+}, {timestamps: false});
+
+/////////////// For VOTES ///////////////
+
+/// DEFAULT GET HERE ///
+app.get('/api/votes', function (req, res) {
+
+    Vote.findAll().then(votes => {
+        res.json(votes)
+    })
+});
+
+/// GET VOTE BY value ///
+app.get('/api/votes/:value', function (req, res) {
+
+    Vote.find({
+        where: {
+            value: req.params.value
+        }
+    }).then((votes) => votes ? res.json(votes) : res.status(404).json({error: 'no votes found'}))
+});
+
+/// POST NEW VOTE ///
+app.post('/api/votes/', function (req, res) {
+
+    Vote.create({
+        value: req.body.value
+    }).then((vote) => res.json(vote))
+});
+
+/// DELETE RECIPE ///
+app.delete('/api/votes/:id', function (req, res) {
+
+    Vote.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then((vote) => vote ? res.json(vote) : res.status(404).json({error: 'unknown vote wi id : ' + req.params.id}))
+});
+
+app.listen(9000);
+console.log('Running on port 9000...');
